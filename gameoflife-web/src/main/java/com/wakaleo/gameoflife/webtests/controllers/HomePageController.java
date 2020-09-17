b1faf8e9e180d91cf89c3a568b09487362b8ac4f
PhanindraReddy <36759669+PhanindraReddyP@users.noreply.github.com>
2020-09-17 19:12:00 +0530

    updated with Vulnerable code in homecontroller.java

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
@@ -23,4 +23,8 @@ public class HomePageController {
     public ModelAndView home() {
         return new ModelAndView("home");
     }
+	
+	public void setValues(String[] somevalues) {
+	this.values = somevalues;
+	}
 }
